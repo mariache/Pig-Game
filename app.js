ba5fd0b57ace8e7765e20abb0d6a2f4d069746c6
SO <omarbagaev@gmail.com>
2019-04-20 03:17:21 +1200

    added init function for a new game

--- app.js
+++ app.js
@@ -10,15 +10,7 @@ GAME RULES:
 */
  var scores, roundScore, activePlayer;
 
-scores =[0, 0];
-roundScore = 0;
-activePlayer = 0;
-
-document.querySelector('.dice').style.display = 'none';
-document.getElementById('score-0').textContent = 0;
-document.getElementById('score-1').textContent = 0;
-document.getElementById('current-0').textContent = 0;
-document.getElementById('current-1').textContent = 0;
+initGame();
 
 document.querySelector('.btn-roll').addEventListener('click', function () {
 
@@ -62,6 +54,25 @@ document.querySelector('.btn-hold').addEventListener('click', function () {
 
 });
 
+function  initGame() {
+    scores =[0, 0];
+    roundScore = 0;
+    activePlayer = 0;
+
+    document.querySelector('.dice').style.display = 'none';
+    document.getElementById('score-0').textContent = 0;
+    document.getElementById('score-1').textContent = 0;
+    document.getElementById('current-0').textContent = 0;
+    document.getElementById('current-1').textContent = 0;
+    document.getElementById('name-0').textContent ='Player 1';
+    document.getElementById('name-1').textContent ='Player 2';
+    document.querySelector('.player-0-panel').classList.remove('winner');
+    document.querySelector('.player-1-panel').classList.remove('winner');
+    document.querySelector('.player-0-panel').classList.remove('active');
+    document.querySelector('.player-1-panel').classList.remove('active');
+    document.querySelector('.player-0-panel').classList.add('active');
+}
+
 function nextPlayer() {
     activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
     roundScore = 0;
@@ -75,3 +86,4 @@ function nextPlayer() {
     document.querySelector('.dice').style.display = 'none';
 
 };
+ document.querySelector('.btn-new').addEventListener('click', initGame);
