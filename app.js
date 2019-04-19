f558c3baa29e399289768b42d6ed54db39fd3d4c
SO <omarbagaev@gmail.com>
2019-04-20 02:14:07 +1200

    added logic for changing players

--- app.js
+++ app.js
@@ -21,8 +21,34 @@ document.getElementById('current-0').textContent = 0;
 document.getElementById('current-1').textContent = 0;
 
 document.querySelector('.btn-roll').addEventListener('click', function () {
+
+    //1. Step
+    //get Random number for dice
     var dice = Math.floor(Math.random() * 6) +1;
-   var diceDom = document.querySelector('.dice');
+    //2.
+    //Display the result
+    var diceDom = document.querySelector('.dice');
     diceDom.style.display = 'block';
     diceDom.src = 'dice-' + dice + '.png';
+
+    //3.
+    //Update the score if the number wasn't equal to 1
+    if (dice !== 1){
+        //Add score
+        roundScore += dice;
+        document.querySelector('#current-' + activePlayer).textContent = roundScore;
+    }else{
+        //Next player
+        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+        roundScore = 0;
+
+        document.getElementById('current-0').textContent = '0';
+        document.getElementById('current-1').textContent = '0';
+
+        document.querySelector('.player-0-panel').classList.toggle('active');
+        document.querySelector('.player-1-panel').classList.toggle('active');
+
+        document.querySelector('.dice').style.display = 'none';
+    }
 });
+
